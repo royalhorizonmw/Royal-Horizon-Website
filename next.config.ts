2b2d1f665778a1faa56a9bcf1567d4879c7ff669
Hope Chidule <royalhorizonmw@gmail.com>
2026-09-07 17:42:24 +0200

    fix: allow published blog images

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const contentSecurityPolicy = [
   "object-src 'none'",
   "script-src 'self' 'unsafe-inline'",
   "style-src 'self' 'unsafe-inline'",
-  "img-src 'self' data: blob:",
+  "img-src 'self' data: blob: https://*.supabase.co",
   "font-src 'self' data:",
   "connect-src 'self'",
   "frame-src https://www.google.com",
